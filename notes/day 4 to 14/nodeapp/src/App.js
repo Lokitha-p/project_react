f7a5e8a4a59b18815ba61d9195aacec23f81e88d
Lokitha <153300379+Lokitha-p@users.noreply.github.com>
2025-01-29 11:55:38 +0530

    Add files via upload

---
+++ notes/day 4 to 14/nodeapp/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import DataComponent from './DataComponent'
+const App = () => {
+  return (
+    <div>
+      <DataComponent />
+    </div>
+  )
+}
+
+export default App;
---
+++ notes/day 4 to 14/nodeapp/src/greetings.js
@@ -0,0 +1,6 @@
+const greetingMessage ='Hello,from the custom module!';
+const Greet = () => {
+ console.log(greetingMessage);
+}
+
+module.exports=Greet;
